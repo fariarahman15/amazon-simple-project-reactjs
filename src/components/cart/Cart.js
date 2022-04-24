670de8b26b9c4f9eeae9b62d77e1f7f3674eb7cb
fariarahman15 <faria.khanam@northsouth.edu>
2022-04-25 03:21:57 +0600

    cart update

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './Cart.css'
+const Cart = (props) => {
+    const {cart} = props;
+    console.log(cart);
 
-const Cart = ({cart}) => {
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+
+    let tax = 0;
+    tax = +(total * 0.15).toFixed(2);
+    console.log(tax);
+
     return (
-        <div>
+        <div className="cart">
             <h3>Order Summery</h3>
             <p>Selected Items: {cart.length}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Charge: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <p>Grand Total: ${total+shipping+tax}</p>
         </div>
     );
 };
